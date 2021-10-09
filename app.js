ea080594f012cc5a4c6404606ec81a55fabb064a
Dennis HG <dennishg32@gmail.com>
2021-10-09 17:05:31 +0200

    edited some errors and added a women to displace to different result

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            firstname: "Dennis",
-            lastname: "HG",
-            email: "dennihg32@gmail.com",
+            firstname: "kaka",
+            lastname: "JR",
+            email: "kaka@gmail.com",
             gender: "male",
-            picture: "https://media.wired.com/photos/5cc244c9af643e2f373ebb28/1:1/w_1800,h_1800,c_limit/Coding-Becomes-Criminal.jpg",
+            picture: "https://randomuser.me/api/portraits/men/10.jpg",
         };
+    },
+    methods: {
+        getUser() {
+            (this.firstname = "Delice"),
+            (this.lastname = "Muhoza"),
+            (this.email = "demuhoza@gmail.com");
+            this.gender = "female";
+            this.picture = "https://randomuser.me/api/portraits/women/10.jpg";
+        },
     },
 });
 app.mount("#app");
