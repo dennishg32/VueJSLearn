760950daa2895a77ed6e878f18aa9f911944b4da
Dennis HG <dennishg32@gmail.com>
2021-10-09 16:28:28 +0200

    added a function having different Data

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const app = Vue.createApp({
-    template: "<h1>Hello World</h1>",
+    data() {
+        return {
+            firstname: "Dennis",
+            lastname: "HG",
+            email: "dennihg32@gmail.com",
+            gender: "male",
+            picture: "https://media.wired.com/photos/5cc244c9af643e2f373ebb28/1:1/w_1800,h_1800,c_limit/Coding-Becomes-Criminal.jpg",
+        };
+    },
 });
 app.mount("#app");
